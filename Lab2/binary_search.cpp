15d4eca7dfaa847474c9ace7d0f32996cff4b8e0
Abhinav <abhinavspl2003@gmail.com>
2025-09-18 23:11:35 +0530

    add binary search

--- Lab2/binary_search.cpp
+++ Lab2/binary_search.cpp
@@ -1,53 +1,37 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
+int BinarySearch(const int numbers[], int arraySize, int targetValue) {
+    int leftIndex = 0;
+    int rightIndex = arraySize - 1;
 
-// Assuming the array provided by the user is already sorted...
+    while (leftIndex <= rightIndex) {
+        int midIndex = leftIndex + (rightIndex - leftIndex) / 2;
 
-
-
-// binary searching
-
-void binarySearch(int array[],int size,int value){
-    int left = 0;
-    int right = size - 1;
-
-    int middle = left + ((left + right)/2);
-
-    while(left < right){
-        
+        if (numbers[midIndex] == targetValue) {
+            return midIndex; 
+        } else if (numbers[midIndex] < targetValue) {
+            leftIndex = midIndex + 1; 
+        } else {
+            rightIndex = midIndex - 1; 
+        }
     }
+
+    return -1; 
 }
 
+int main() {
+    int sortedNumbers[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int numberOfElements = sizeof(sortedNumbers) / sizeof(sortedNumbers[0]);
+    int numberToFind = 23;
 
+    int resultIndex = BinarySearch(sortedNumbers, numberOfElements, numberToFind);
 
-int main(){
-
-    int size,userChoice;
-    cout << "Enter Your size of Array" << endl;
-    cin >> size;
-
-    int array[size];
-    for(int i = 0; i < size; i++){
-        cout << "Enter your array elements one by one" << endl;
-        cin >> array[i];
+    if (resultIndex != -1) {
+        cout << "The number " << numberToFind << " was found at index " << resultIndex << "." << endl;
+    } else {
+        cout << "The number " << numberToFind << " was not found in the array." << endl;
     }
-    cout << "You want to do Binary Search...(1 for Yes, 0 for No)" << endl;
-    cin >> userChoice;
-
-    switch (userChoice)
-    {
-    case 1:
-        cout << "What you want to seach...ENTER A VALUE" << endl;
-        binarySearch(array,size,value);
-        break;
-    
-    default:
-        break;
-    }
-
-
-
 
     return 0;
 }
